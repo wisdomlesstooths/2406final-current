cc4562ad172d3fbefb9779938a41344b10219fde
Ellie <82733739+wisdomlesstooths@users.noreply.github.com>
2021-04-18 19:51:29 -0400

    yet again chloe dies

--- api/routes/reviews.js
+++ api/routes/reviews.js
@@ -1,25 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const Review = require("./ReviewModel");
 
 // GET /api/users/
 router.get('/', (req, res) => {
-    res.send('Get List Of Reviews');
+  res.send('Get List Of Reviews');
 })
+//have to make sendReview
 
-// GET /api/users/:name
-router.get('/:id', (req, res) => {
-    const review_id = req.params.id;
-    res.send(`Get Review with id ${review_id}`);
+// GET /api/reviews
+router.get('/:id', async (req, res) => {
+  let review_id = req.params.id;
+  let review = await Review.findById(review_id);
+  //res.send(review);
+  res.render('/review', {
+    review: review
+  })
 })
 
 // POST /api/users
-router.post('/', (req, res) => {
-    let movie = req.body.movie;
+router.post('/', async (req, res) => {
+    let movieName = req.body.movie;
+    //let movie= async get movie object; unless we change model
     let summary = req.body.summary;
     let description = req.body.description;
     let rating = req.body.rating;
-
-    res.send(`${movie}, ${summary}, ${description}, ${rating}`);
+    let user = await User.find().where("loggedIn").equals(true);
+    let r = new Review({
+      //movie: movie,
+      rating: rating,
+      summary: summary,
+      review: description,
+      user: user
+    });
+    r.save(function(err, result){
+      if(err){
+        console.log(err);
+        res.status(500).send("Error creating review.");
+        return;
+      }
+      res.status(201).send(JSON.stringify(u));
+    })
+    res.render('/review', {
+      review: r
+    })
 })
 
 // PUT /api/user/:username
@@ -33,7 +57,7 @@ router.put('/:id', (req, res) => {
     res.send(`${id}, ${movie}, ${summary}, ${description}, ${rating}`);
 })
 
-// DELETE /api/user/:username 
+// DELETE /api/user/:username
 router.delete('/:id', (req, res) => {
     let id = req.params.id;
 
